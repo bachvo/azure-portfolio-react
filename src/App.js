b5020d064ce0122f7a61ca052e4dd92dc9db85f9
Bach Vo <bach_vo@hotmail.com>
2020-04-02 20:49:33 -0700

    add lite call and loading spinners

--- src/App.js
+++ src/App.js
@@ -14,13 +14,38 @@ export default class App extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
-      model: {}
+      model: {},
     };
   }
 
   componentDidMount() {
-    const mainModel = `${API_HOST}/api/main`;
-    fetch(mainModel)
+    const fullModel = `${API_HOST}/api/main`;
+    const liteModel = `${API_HOST}/api/main-lite`;
+
+    fetch(liteModel)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          // if full model returns first, do not reset model
+          if (!this.state.model.profile) {
+            this.setState({
+              isLoaded: true,
+              model: result.data
+            });
+          }
+        },
+        (error) => {
+          // if full model returns first, do not show error
+          if (!this.state.model.profile) {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        }
+      )
+
+    fetch(fullModel)
       .then(res => res.json())
       .then(
         (result) => {
@@ -41,14 +66,24 @@ export default class App extends React.Component {
   render() {
     const { error, isLoaded, model } = this.state;
     if (error) {
-      return <div>Error: {error.message}</div>;
+      return (
+        <div className="alert alert-danger" role="alert">
+          Error: {error.message}
+        </div>
+      );
     } else if (!isLoaded) {
-      return <div>Loading...</div>;
+      return (
+        <div className="text-center my-5">
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      );
     } else {
       return (
         <Router>
           <ScrollToTop />
-          <Nav collection={model.contactInfo} />
+          {model.contactInfo && <Nav collection={model.contactInfo} />}
           <main className="overflow-x-hidden">
             <Switch>
               <Route 
@@ -58,15 +93,15 @@ export default class App extends React.Component {
               />
               <Route 
                 path="/workexp/:id" 
-                render={props => (<CardDetail {...props} collection={model.workExp.cards} type={ANCHOR.WORKEXP}/>)}
+                render={props => (model.workExp && <CardDetail {...props} collection={model.workExp.cards} type={ANCHOR.WORKEXP}/>)}
               />
               <Route 
                 path="/projects/:id" 
-                render={props => (<CardDetail {...props} collection={model.projects.cards} type={ANCHOR.PROJECTS}/>)}
+                render={props => (model.projects && <CardDetail {...props} collection={model.projects.cards} type={ANCHOR.PROJECTS}/>)}
               />
               <Route component={PageNotFound} />
             </Switch>
-            <Footer collection={model.contactInfo} fullName={model.profile.fullName} />
+            {model.profile && <Footer collection={model.contactInfo} fullName={model.profile.fullName} />}
           </main>
         </Router>
       );
--- src/components/home.js
+++ src/components/home.js
@@ -5,59 +5,83 @@ import { ANCHOR } from '../utils/constants';
 
 export default class Home extends React.Component {
   render() {
+    const loadingElement = (
+      <div className="text-center my-5">
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
+
     return (
       <div>
-        <a className="nav__anchor" id={ANCHOR.HOME}></a>
-        <section className="home__intro d-flex flex-column align-items-md-start align-items-lg-center px-2 py-4 p-md-5">
-          <h2>{this.props.model.intro.title}</h2>
-          <p className="max-width-700-px text-md-left text-lg-center pb-4" dangerouslySetInnerHTML={createMarkup(this.props.model.intro.description)}></p>
-          <img alt="" className="home__landing-page-image" src="images/landing_page.png"/>
-        </section>
+        {this.props.model.intro && (
+          <div>
+            <a className="nav__anchor" id={ANCHOR.HOME}></a>
+            <section className="home__intro d-flex flex-column align-items-md-start align-items-lg-center px-2 py-4 p-md-5">
+              <h2>{this.props.model.intro.title}</h2>
+              <p className="max-width-700-px text-md-left text-lg-center pb-4" dangerouslySetInnerHTML={createMarkup(this.props.model.intro.description)}></p>
+              <img alt="" className="home__landing-page-image" src="images/landing_page.png"/>
+            </section>
+          </div>
+        )}
 
-        <a className="nav__anchor" id={ANCHOR.ABOUT}></a>
-        <section className="background-section d-flex flex-column align-items-md-start align-items-lg-left px-2 py-4 p-md-5">
-          <div className="container d-flex flex-column">
+        {!this.props.model.profile && loadingElement}
+
+        {this.props.model.profile && (
+          <div>
+            <a className="nav__anchor" id={ANCHOR.ABOUT}></a>
+            <section className="background-section d-flex flex-column align-items-md-start align-items-lg-left px-2 py-4 p-md-5">
+              <div className="container d-flex flex-column">
+                <div className="d-flex row row-cols-1 row-cols-md-3">
+                  <div className="card h-100 col-md-3 p-0">
+                    <img className="card-img-top" alt="" src={this.props.model.profile.imgSrc} width="200" />
+                    <div className="card-body">
+                      <h5 className="card-title">{this.props.model.profile.fullName}</h5>
+                      <p className="card-text">{this.props.model.profile.occupation}</p>
+                    </div>
+                  </div>
+                  <div className="col-md-9 pt-4 pt-md-0 pl-0 pl-md-5">
+                    <h2>{this.props.model.profile.fullName}</h2>
+                    <p dangerouslySetInnerHTML={createMarkup(this.props.model.profile.description)}></p>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        )}
+
+        {this.props.model.skills && (
+          <section className="d-flex flex-column align-items-md-start align-items-lg-center px-2 py-4 p-md-5">
             <div className="d-flex row row-cols-1 row-cols-md-3">
-              <div className="card h-100 col-md-3 p-0">
-                <img className="card-img-top" alt="" src={this.props.model.profile.imgSrc} width="200" />
-                <div className="card-body">
-                  <h5 className="card-title">{this.props.model.profile.fullName}</h5>
-                  <p className="card-text">{this.props.model.profile.occupation}</p>
-                </div>
-              </div>
-              <div className="col-md-9 pt-4 pt-md-0 pl-0 pl-md-5">
-                <h2>{this.props.model.profile.fullName}</h2>
-                <p dangerouslySetInnerHTML={createMarkup(this.props.model.profile.description)}></p>
-              </div>
+              {this.props.model.skills.map((item, key) => {
+                return (
+                  <div key={key} className="d-flex flex-column align-items-md-start align-items-lg-center col">
+                    <img src={item.imgSrc} height="200" width="200" alt=""/>
+                    <h3 className="mt-3">{item.title}</h3>
+                    <p>{item.description}</p>
+                  </div>
+                );
+              })}
             </div>
-          </div>
-        </section>
+          </section>
+        )}
 
-        <section className="d-flex flex-column align-items-md-start align-items-lg-center px-2 py-4 p-md-5">
-          <div className="d-flex row row-cols-1 row-cols-md-3">
-            {this.props.model.skills.map((item, key) => {
-              return (
-                <div key={key} className="d-flex flex-column align-items-md-start align-items-lg-center col">
-                  <img src={item.imgSrc} height="200" width="200" alt=""/>
-                  <h3 className="mt-3">{item.title}</h3>
-                  <p>{item.description}</p>
-                </div>
-              );
-            })}
-          </div>
-        </section>
-
-        <CardCollection
-          classNames="home__workexp"
-          id={ANCHOR.WORKEXP}
-          model={this.props.model.workExp}
-        />
-
-        <CardCollection
-          classNames="background-section"
-          id={ANCHOR.PROJECTS}
-          model={this.props.model.projects}
-        />
+        {this.props.model.workExp && (
+          <CardCollection
+            classNames="home__workexp"
+            id={ANCHOR.WORKEXP}
+            model={this.props.model.workExp}
+          />
+        )}
+        
+        {this.props.model.projects && (
+          <CardCollection
+            classNames="background-section"
+            id={ANCHOR.PROJECTS}
+            model={this.props.model.projects}
+          />
+        )}
       </div>
     );
   }
