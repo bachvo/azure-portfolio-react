97bfbba0fb6e97676f16f5f0337977419d315588
Bach Vo <bach_vo@hotmail.com>
2022-12-31 17:45:15 -0800

    create MarkUp type for return

--- src/utils/create-markup.tsx
+++ src/utils/create-markup.tsx
@@ -1,3 +1,7 @@
+type MarkUp = {
+  __html: string
+}
+
 /**
  * Generates an object with the `__html` so dangerouslySetInnerHTML
  * can consume and serialize.
@@ -5,6 +9,6 @@
  * @param {string} text - the text that may contain markup
  * @return {object} returns an object so dangerouslySetInnerHTML can consume
  */
-export function createMarkup(text: string): object {
+export function createMarkup(text: string): MarkUp {
   return { __html: text };
 }
